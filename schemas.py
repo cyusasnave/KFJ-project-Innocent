622de78ef0579a57e8154a684daa6e22a49ff227
albertmus123 <mualbert2@gmail.com>
2024-03-09 15:30:09 +0200

    initial commit of fastapi backend

---
+++ schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, model_validator
+from uuid import UUID
+from fastapi import HTTPException , status
+
+class UserModel(BaseModel):
+    username: str
+    password: str
+    
+    @model_validator(mode="after")
+    def length_of_password(self):
+        if len(self.password) < 8 :
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Password must be at least 8 characters")
+        return self
+    
+class UserView(BaseModel):
+    id: UUID
+    username: str
+    
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
