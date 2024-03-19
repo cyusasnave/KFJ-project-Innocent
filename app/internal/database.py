1f22d776ff24f08084b4ee1155097b9a9a5f69c6
albertmus123 <mualbert2@gmail.com>
2024-03-19 14:16:56 +0200

    add database configuration in .env file

--- app/internal/database.py
+++ app/internal/database.py
@@ -3,8 +3,16 @@ from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 from fastapi import Depends
 from typing import Annotated
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:123@localhost:5432/jobFinder"
+load_dotenv()
+
+db_user = os.getenv("POSTGRESQL_USERNAME")
+db_pass = os.getenv("POSTGRESQL_PASSWORD")
+
+
+DATABASE_URL = f"postgresql://{db_user}:{db_pass}@localhost:5432/jobFinder"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
