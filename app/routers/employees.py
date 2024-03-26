587c2732e7c4550a153259a853d9c9301a3ffe81
innox123 <innocentcyusa2@gmail.com>
2024-03-26 17:35:48 +0200

    Changes

--- app/routers/employees.py
+++ app/routers/employees.py
@@ -22,20 +22,32 @@ async def add_employee_profile(
     current_user: Annotated[User, Depends(get_current_employee)],
     db: Session = Depends(get_db),
 ):
-    user = Employee(
-        user_id=current_user.id,
-        user_specialization=specialization_id,
-        first_name=request.first_name,
-        last_name=request.last_name,
-        phone=request.phone,
-        cv_url="null",
-        profile_url="null",
-        province=request.province,
-        district=request.district,
-        sector=request.sector,
-        cell=request.cell,
-        village=request.village,
-    )
+    user = db.query(Employee).filter(Employee.user_id == current_user.id).first()
+    if user:
+        user.user_specialization=specialization_id,
+        user.first_name=request.first_name,
+        user.last_name=request.last_name,
+        user.phone=request.phone,
+        user.province=request.province,
+        user.district=request.district,
+        user.sector=request.sector,
+        user.cell=request.cell,
+        user.village=request.village,
+    else:
+        user = Employee(
+            user_id=current_user.id,
+            user_specialization=specialization_id,
+            first_name=request.first_name,
+            last_name=request.last_name,
+            phone=request.phone,
+            cv_url="null",
+            profile_url="null",
+            province=request.province,
+            district=request.district,
+            sector=request.sector,
+            cell=request.cell,
+            village=request.village,
+        )
     db.add(user)
     db.commit()
     db.refresh(user)
@@ -54,25 +66,36 @@ async def get_employee_profile(
         raise HTTPException(status_code=404, detail="Employee Profile not found")
     return profile
 
-@router.post("/api/v1/employee/add/cv_profile")
+@router.post("/api/v1/employee/add/cv_file")
 async def add_employee_cv_picture(
     current_user: Annotated[User, Depends(get_current_employee)],
     db: Session = Depends(get_db),
     cv_file: UploadFile = File(...),
-    image_file: UploadFile = File(...),
 ):
     employee = db.query(Employee).filter(Employee.user_id == current_user.id).first()
     if employee is None:
         raise HTTPException(status_code=404, detail="Employee Not Found!")
     cv_file_path = save_uploaded_cv(cv_file)
-    image_file_path = save_uploaded_picture(image_file)
     employee.cv_url = cv_file_path
+    db.commit()
+    return "Your CV uploaded SuccessFully!"
+
+@router.post("/api/v1/employee/add/profile_picture")
+async def add_employee_cv_picture(
+    current_user: Annotated[User, Depends(get_current_employee)],
+    db: Session = Depends(get_db),
+    profile_picture: UploadFile = File(...),
+):
+    employee = db.query(Employee).filter(Employee.user_id == current_user.id).first()
+    if employee is None:
+        raise HTTPException(status_code=404, detail="Employee Not Found!")
+    image_file_path = save_uploaded_picture(profile_picture)
     employee.profile_url = image_file_path
     db.commit()
-    return "Your Files uploaded SuccessFully!"
+    return "Profile picture uploaded SuccessFully!"
 
 
-@router.post("/api/v1/specialization/data/")
+@router.post("/api/v1/specialization/add/")
 async def add_specialization(
     request: SpecializationModel, db: Session = Depends(get_db)
 ):
@@ -83,7 +106,7 @@ async def add_specialization(
     return f"{new_data.name} is created Successfully!"
 
 
-@router.get("/api/v1/specialization/data/all", response_model=List[SpecializationView])
+@router.get("/api/v1/specialization/all", response_model=List[SpecializationView])
 async def get_all_specialization(db: Session = Depends(get_db)):
     specialization = db.query(Specialization).all()
     return specialization
